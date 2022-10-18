36d0c79b58065cdfb7e4fc2fb995aabb179757cb
Adewale Salami <adewalesalami@rocketmail.com>
2022-10-17 17:03:42 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+/*
+ *File: 4-print_alphabt.c
+** Auth: Wale
+*/
+
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, except for q and e.
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
