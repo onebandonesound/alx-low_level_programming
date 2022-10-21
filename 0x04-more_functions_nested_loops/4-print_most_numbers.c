4b062a1e74b569d31154c663cbe7b0efc6266f8b
Adewale Salami <adewalesalami@rocketmail.com>
2022-10-21 13:19:15 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers
+ * Return: always 0 success
+ */
+void print_most_numbers(void);
+{
+	int c = 0;
+
+	for (c = 0; c <= 9; c++)
+	{
+		if (c != 2 && c != 4)
+			_putchar(c + 48);
+	}
+	_putchar("\n")
+}
